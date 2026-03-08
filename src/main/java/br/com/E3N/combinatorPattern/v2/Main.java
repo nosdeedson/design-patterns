b132962253b6cba783d77cf7ab99c3e9c2676d54
nosdeedson <josemariazumira@gmail.com>
2026-03-08 11:22:42 -0300

    added a definition of the pattern

--- src/main/java/br/com/E3N/combinatorPattern/v2/Main.java
+++ src/main/java/br/com/E3N/combinatorPattern/v2/Main.java
@@ -2,6 +2,13 @@ package br.com.E3N.combinatorPattern.v2;
 
 public class Main {
 
+    /**
+     * The combinator pattern is similar with the builder pattern in a way
+     * the builder help to create an object meanwhile the combinator allow you to add behavior to the object
+     * The combinator pattern allow you to create a complex behavior by composing small functions that return
+     * the same type, so you can do something like this object.validateThis().validateThat().validateSomething()
+     * and receive the result.
+     */
     public static void main(String[] args) {
         App.execute();
     }
